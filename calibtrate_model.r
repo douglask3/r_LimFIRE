abbf9d0f3469dfba2c21644fcc0bd95ac369dedb
douglask3 <douglas.i.kelley@gmail.com>
2016-06-24 18:14:36 +0100

    some sudo files for making inputs from raw adata and callibrating model. Not got anything in yet

---
+++ calibtrate_model.r
@@ -0,0 +1,2 @@
+
+source('make_model_inputs.r)
---
+++ make_model_inputs.r
@@ -0,0 +1,16 @@
+source('cfg.r')
+
+
+fire_file = 
+
+fuel_file =
+
+live_moisture_file =
+dead_moisture_file =
+
+lightning_file = 
+pasture_file = 
+pop_dens_file = 
+
+cropland_file =
+urban_file =
