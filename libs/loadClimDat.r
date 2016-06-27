b934e3f6ca260c2bb7bd5df61a06d4c0cfff9eb4
douglask3 - Reading <douglas.i.kelley@gmail.com>
2016-06-27 19:36:28 +0100

    correting typo

--- libs/loadClimDat.r
+++ libs/loadClimDat.r
@@ -1,4 +1,4 @@
-loadClimDat <- function(dir, varns, clim_layers {
+loadClimDat <- function(dir, varns, clim_layers) {
 	files = list.files(dir, full.names = TRUE)
 
 	loadDat <- function(varn) {
@@ -9,6 +9,6 @@ loadClimDat <- function(dir, varns, clim_layers {
 	dat = lapply(varns, loadDat)
 
 	nyears = floor(length(clim_layers)/12)
-	
+
 	return(list(dat, nyears))
 }
