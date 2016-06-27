a8ae8e71ff0ebfd576de4ce748333c7d293a0abb
doug <douglas.kelley@students.mq.edu.au>
2016-06-27 19:32:41 +0100

    adding oherpackages that need initailisation@

--- initalise.r
+++ initalise.r
@@ -1,3 +1,10 @@
 install.packages('devtools')
 library('devtools')
 install_github("rhyswhitley/r_stash/rstash")
+
+install_bitbucket('douglask3/gitProjectExtras/gitBasedProjects')
+install_github('douglask3/benchmarkmetrics/benchmarkMetrics')
+install_bitbucket('douglask3/rasterExtraFuns/rasterExtras')
+install_bitbucket('douglask3/rasterExtraFuns/rasterPlotFunctions')
+install.packages('raster')
+install.packages('mapdata')
