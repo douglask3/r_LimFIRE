915fc578782e35e3144dd1d7506ac5805dfd703c
Douglas Kelley <douglas.i.kelley@gmail.com>
2016-07-01 14:10:58 +0100

    adding stuff required for plot raster package (this should go into plot raster package at some poiint

--- initalise.r
+++ initalise.r
@@ -1,10 +1,17 @@
 install.packages('devtools')
 library('devtools')
+
+install.packages('raster')
+install.packages('colorspace')
+install.packages('spam')
+install.packages('fields')
+install.packages('mapproj')
+install.packages('mapdata')
+
 install_github("rhyswhitley/r_stash/rstash")
 
 install_bitbucket('douglask3/gitProjectExtras/gitBasedProjects')
 install_github('douglask3/benchmarkmetrics/benchmarkMetrics')
 install_bitbucket('douglask3/rasterExtraFuns/rasterExtras')
 install_bitbucket('douglask3/rasterExtraFuns/rasterPlotFunctions')
-install.packages('raster')
-install.packages('mapdata')
+
