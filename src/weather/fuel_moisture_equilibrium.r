eb58ff31cd66f4fdf1351fced85ba2dc2b55ab6c
Douglas Kelley <douglas.i.kelley@gmail.com>
2016-07-01 16:04:27 +0100

    using a simpler index, which also seems to be more stable across Ts and Hs. Sharples, J. J., McRae, R. H. D., Weber, R. O., and Gill, A. M.: A simple index for assessing fuel moisture content, Environ. Modell. Softw., 24, 637–646, doi:10.1016/j.envsoft.2008.10.012, 2009.

--- src/weather/fuel_moisture_equilibrium.r
+++ src/weather/fuel_moisture_equilibrium.r
@@ -1,27 +1,8 @@
 fuel_moisture_equilibrium <- function(Pr, Hr, T) {
-    
-    m_eq_test = m_eq(Hr - 0.01, T)
-    m_eq      = m_eq(Hr       , T)
-       
-    m_eq[m_eq_test > m_eq] = 0
-    m_eq[Hr == 0] = 0
-
+    m_eq = 10 - 0.25 *(T - Hr)
     m_eq[Pr > 3] = 100
 
-    m_eq[m_eq > 100] = 100
-    m_eq[m_eq < 0] = 0
-
-    
-    m_eq = 10 - 0.25 *(T - Hr)
     return(m_eq)
 }
 
-m_eq <- function(Hr, T)  m_eq1(Hr) + m_eq2(Hr) + m_eq3(Hr, T )
 
-
-
-m_eq1 <- function(Hr) 0.942 * Hr ^ (-.679)
-
-m_eq2 <- function(Hr) 0.000499 * exp (0.1 * Hr)
-
-m_eq3 <- function(Hr, T) 0.18 * (21.1 - T) * (1 - exp(-0.115 * Hr))
