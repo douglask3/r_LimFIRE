e725b72fb93cdc63682db0fcd6631b75c75ad4d9
Douglas Kelley <douglas.i.kelley@gmail.com>
2016-07-01 14:12:09 +0100

    atepmt at introdiucing wet day = 100 emc days

--- make_emc.r
+++ make_emc.r
@@ -7,7 +7,8 @@ sourceAllLibs('src/weather/')
 
 ## paths and parameters
 dir   = 'data/cru_ts3.23/'
-varns = c(vap    = 'vap',
+varns = c(wetday = 'wet',
+          vap    = 'vap',
           temp   = 'tmp')
 
 clim_layers = 73:288
@@ -22,10 +23,15 @@ c(dat, nyears) := loadClimDat
 ################################################################################
 
 make_emc <- function(i) {
-	Vap = dat[[1]][[i]]
-	Tas = dat[[2]][[i]]
-	Hr = realtive_humidity(Tas, Vap)
+        Wet = dat[['wetday']][[i]]
+	Vap = dat[['vap'   ]][[i]]
+	Tas = dat[['temp'  ]][[i]]
+	Hr = realtivi_humidity(Tas, Vap)
 	emc = fuel_moisture_equilibrium(0, Hr, Tas)
+        browser()
+        emc = emc * (1-Wet) + 100 * Wet
+        
+        return(emc)
 }
 
 
