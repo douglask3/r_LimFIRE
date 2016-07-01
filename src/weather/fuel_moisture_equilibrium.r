6f1fcff4dd9b4ac1c8fe7966a91895fb56c8cc82
Douglas Kelley <douglas.i.kelley@gmail.com>
2016-07-01 16:02:24 +0100

    making changes in fuel moisture calculation: Pr is now a raster as well, and sensativity test by assess_emc suugest that when lower Hr results in higher emc, then emc should be zero. SO that was implemented too

--- src/weather/fuel_moisture_equilibrium.r
+++ src/weather/fuel_moisture_equilibrium.r
@@ -1,14 +1,26 @@
 fuel_moisture_equilibrium <- function(Pr, Hr, T) {
-	if (Pr <= 3.0) m_eq = m_eq1(Hr) + m_eq2(Hr) + m_eq3(Hr, T )
-		else {
-			m_eq   = Hr
-			m_eq[] = 100
-		}
-	m_eq[m_eq > 100] = 100; m_eq[m_eq < 0] = 0
-	return(m_eq)
+    
+    m_eq_test = m_eq(Hr - 0.01, T)
+    m_eq      = m_eq(Hr       , T)
+       
+    m_eq[m_eq_test > m_eq] = 0
+    m_eq[Hr == 0] = 0
+
+    m_eq[Pr > 3] = 100
+
+    m_eq[m_eq > 100] = 100
+    m_eq[m_eq < 0] = 0
+
+    
+    m_eq = 10 - 0.25 *(T - Hr)
+    return(m_eq)
 }
 
-m_eq1 <- function(Hr) 0.942 * Hr ^ -.679
+m_eq <- function(Hr, T)  m_eq1(Hr) + m_eq2(Hr) + m_eq3(Hr, T )
+
+
+
+m_eq1 <- function(Hr) 0.942 * Hr ^ (-.679)
 
 m_eq2 <- function(Hr) 0.000499 * exp (0.1 * Hr)
 
