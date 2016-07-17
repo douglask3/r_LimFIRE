52cf523a8b2eb36595591385a45962c4dca99785
douglask3 <douglas.i.kelley@gmail.com>
2016-07-17 22:03:38 +0100

    correcting clim layers index

--- cfg.r
+++ cfg.r
@@ -10,7 +10,7 @@ setupProjectStructure(dirn = c("outputs", "data", "temp", "figs"))
 sourceAllLibs('src/libs')
 
 years       = 2000:2014
-clim_layers =  127:294
+clim_layers =  115:282
 
 ml = c(31,28,31,30,31,30,31,31,30,31,30,31)
 drive_fname = c(alpha   = 'alpha',
