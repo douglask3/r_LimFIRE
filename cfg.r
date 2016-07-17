81298e97f985039c7b338cfc20604a785342a6ea
douglask3 <douglas.i.kelley@gmail.com>
2016-07-17 16:02:10 +0100

    shifiting clim timing to match up with npp obs

--- cfg.r
+++ cfg.r
@@ -10,7 +10,7 @@ setupProjectStructure(dirn = c("outputs", "data", "temp", "figs"))
 sourceAllLibs('src/libs')
 
 years       = 2000:2014
-clim_layers =  121:288
+clim_layers =  127:294
 
 ml = c(31,28,31,30,31,30,31,31,30,31,30,31)
 drive_fname = c(alpha   = 'alpha',
