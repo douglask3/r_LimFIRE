8d99f8322852522ef257163f9c32b66ff3879dae
douglask3 <douglas.i.kelley@gmail.com>
2016-07-23 18:04:23 +0100

    fixing typem allowing return of fire only, and normalising M H P contribution parameters to make initial choice and boundds easier to constrain in nls

--- src/LimFIRE/LimFIRE.r
+++ src/LimFIRE/LimFIRE.r
@@ -1,23 +1,24 @@
 LimFIRE <- function(fuel, moisture_live, moisture_dead,
                     lightning, human_ignitions,
                     agriculture, popdens,
-                    f1, f2, M, m1, m2, H, i1, P, s1, s2) {
+                    f1, f2, M, m1, m2, H, i1, P, s1, s2, fireOnly = FALSE) {
 
-    moisture   = moisture_live + M * moisture_dead
-    ignitions  = lightning + H * human_ignitions
-    supression = agriculture + P * popdens
-
+    moisture   = (moisture_live + M * moisture_dead) / (1 + M)
+    ignitions  = (lightning + H * human_ignitions) / (1 + H)
+    supression = (agriculture + P * popdens) / (1 + P)
+    
     Fuel       = LimFIRE.fuel      (fuel      , f1, f2)
     Moisture   = LimFIRE.moisture  (moisture  , m1, m2)
     Ignitions  = LimFIRE.ignitions (ignitions , i1    )
     Supression = LimFIRE.supression(supression, s1, s2)
 
     Fire = (1 - Fuel) * (1 - Moisture) * (1 - Ignitions) * (1 - Supression)
-
-    return(list(Fire, Fuel, Moisture, Ingitions, Supression))
+    
+    if (fireOnly) return(Fire)
+    return(list(Fire, Fuel, Moisture, Ignitions, Supression))
 }
 
 LimFIRE.fuel       <- function(...) 1-f1(...)
 LimFIRE.moisture   <- function(...)   f1(...)
-LimFIRE.ignitions  <- function(...) 1-f3(...)
+LimFIRE.ignitions  <- function(...) 1-f2(...)
 LimFIRE.supression <- function(...)   f1(...)
