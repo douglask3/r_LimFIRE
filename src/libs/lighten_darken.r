76428c8ea5615cfa7c7c89dbb76cb62ed113a1d1
douglask3 <douglas.i.kelley@gmail.com>
2016-07-26 23:06:34 +0100

    implementing 2nd lighten funvtion from following site to stop breaking: https://gist.github.com/Jfortin1/72ef064469d1703c6b30

--- src/libs/lighten_darken.r
+++ src/libs/lighten_darken.r
@@ -8,6 +8,6 @@ darken <- function(color, factor=1.4){
 lighten <- function(color, factor=1.4){
     col <- col2rgb(color)
     col <- col*factor
-    col <- rgb(t(col), maxColorValue=255)
+    col <- rgb(t(as.matrix(apply(col, 1, function(x) if (x > 255) 255 else x))), maxColorValue=255)
     col
 }
