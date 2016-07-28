8bc4154ce2c04ef296042ce2f7e5123d79049fe3
douglask3 <douglas.i.kelley@gmail.com>
2016-07-28 23:10:54 +0100

    moving fig labels to top of script

--- plot_human_contribution.r
+++ plot_human_contribution.r
@@ -3,6 +3,19 @@ mod_file = 'outputs/LimFIRE_fire'
 fig_file = 'figs/HumanImpactMap.png'
 
 mod_file = paste(mod_file, c('', 'noHumanIngnitions', 'noHumans'), '.nc', sep ='')
+
+
+diff_lims  = c(-30, -20, -10, -5, -1, 0, 1) 
+diff_cols  = c('#000033', '#0099FF', '#66FFFF', '#FFEE00')
+
+cont_lims1 = c(0, 2, 5, 10, 20, 40, 60, 80)
+cont_cols1 = fire_cols
+
+cont_lims2 = cont_lims1
+cont_cols2 = c("#FFFFFF", "#00EEFF", "#0022AA", "#000033") 
+
+labs = c('a) Full model burnt area', 'b) No human ignitions', 'c) No humans', 'd) Burnt Area from human ignitions', 'e) Contribution of humans',
+         'f) % Contribution of human igntions', 'g) % Contribution of humans') 
  
 control = runIfNoFile(mod_file[1], runLimFIREfromstandardIns, fireOnly = TRUE)
 noIgnit = runIfNoFile(mod_file[2], runLimFIREfromstandardIns, fireOnly = TRUE, 
@@ -23,14 +36,7 @@ layout(rbind(c(1,  2,  3),
              c(0, 10, 12)),
              height = c(1, 0.3, 1, 0.3, 1, 0.3))
 
-diff_lims  = c(-30, -20, -10, -5, -1, 0, 1) 
-diff_cols  = c('#000033', '#0099FF', '#66FFFF', '#FFEE00')
 
-cont_lims1 = c(0, 2, 5, 10, 20, 40, 60, 80)
-cont_cols1 = fire_cols
-
-cont_lims2 = cont_lims1
-cont_cols2 = c("#FFFFFF", "#00EEFF", "#0022AA", "#000033") 
 
 mtextStandard <- function(...) mtext(...)
 
@@ -45,24 +51,26 @@ standard_legend <- function(cols = fire_cols, lims = fire_lims, dat,
 standard_legend2 <- function(...)
         standard_legend(plot_loc = c(0.2, 0.9, 0.65, 0.78), ...)
                   
+                               
+
 control = aaConvert(control)
-mtextStandard('a) Full model burnt area')
+mtextStandard(labs[1])
 noIgnit = aaConvert(noIgnit)
-mtextStandard('b) No human ignitions')
+mtextStandard(labs[2])
 noAnyth = aaConvert(noAnyth)
-mtextStandard('c) No humans')
+mtextStandard(labs[3])
 
 standard_legend(dat = control)
 
 
 noIgnit = control - noIgnit
-noAnythi = control - noAnyth
+noAnythi = control - noAnyth  
 
 plot_raster(noIgnit, fire_lims /10)
-mtextStandard('d) Burnt Area from human ignitions')
+mtextStandard(labs[4])
 standard_legend2(dat = noIgnit, lims = fire_lims/10)
 plot_raster(noAnythi, diff_lims, diff_cols)
-mtextStandard('e) Contribution of humans')
+mtextStandard(labs[5])
 standard_legend2(diff_cols, diff_lims, dat = noAnyth)
 
 
@@ -70,10 +78,10 @@ noIgnit = 100 * noIgnit / control
 noAnyth = 100 * control / noAnyth
 
 plot_raster(noIgnit, cont_lims1, cont_cols1)
-mtextStandard('f) % Contribution of human igntions')
+mtextStandard(labs[6])
 standard_legend2(cont_cols1, cont_lims1, dat = noIgnit)
 plot_raster(noAnyth, cont_lims2, cont_cols2)
-mtextStandard('g) % Contribution of humans')
+mtextStandard(labs[7])
 standard_legend2(cont_cols2, cont_lims2, dat = noAnyth)
 
 dev.off.gitWatermark()
