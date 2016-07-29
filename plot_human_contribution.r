55ce78d686d1047d754b79ad6392f60b7d164be1
douglask3 <douglas.i.kelley@gmail.com>
2016-07-29 21:44:05 +0100

    plttint human ignitrion and human impact on same colour scale

--- plot_human_contribution.r
+++ plot_human_contribution.r
@@ -5,8 +5,12 @@ fig_file = 'figs/HumanImpactMap.png'
 mod_file = paste(mod_file, c('', 'noHumanIngnitions', 'noHumans'), '.nc', sep ='')
 
 
-diff_lims  = c(-30, -20, -10, -5, -1, 0, 1) 
-diff_cols  = c('#000033', '#0099FF', '#66FFFF', '#FFEE00')
+diff_lims1 = c(0, 0.1, 1, 2, 5) 
+diff_cols1 = fire_cols
+
+diff_lims2 = c(-20, -10, -5, -2, -1, -0.1, 0.1, 1, 2, 5) 
+diff_cols2 = c('#000033', '#0022AA',  '#00EEFF', 'white', '#FFEE00', '#AA2200')
+
 
 cont_lims1 = c(0, 2, 5, 10, 20, 40, 60, 80)
 cont_cols1 = fire_cols
@@ -30,10 +34,10 @@ par(mar = c(0,0,0,0))
                  
 layout(rbind(c(1,  2,  3),
              c(4,  4,  4), 
-             c(0,  5,  7), 
-             c(0,  6,  8),
-             c(0,  9, 11),
-             c(0, 10, 12)),
+             c(0,  5,  6), 
+             c(0,  7,  7),
+             c(0,  8, 10),
+             c(0,  9, 11)),
              height = c(1, 0.3, 1, 0.3, 1, 0.3))
 
 
@@ -66,12 +70,13 @@ standard_legend(dat = control)
 noIgnit = control - noIgnit
 noAnythi = control - noAnyth  
 
-plot_raster(noIgnit, fire_lims /10)
+plot_raster(noIgnit, diff_lims2,diff_cols2)
 mtextStandard(labs[4])
-standard_legend2(dat = noIgnit, lims = fire_lims/10)
-plot_raster(noAnythi, diff_lims, diff_cols)
+#standard_legend2(diff_cols1, diff_lims1, dat = noIgnit)
+plot_raster(noAnythi, diff_lims2, diff_cols2)
 mtextStandard(labs[5])
-standard_legend2(diff_cols, diff_lims, dat = noAnyth)
+#standard_legend2(diff_cols2, diff_lims2, dat = noAnyth)
+standard_legend2(diff_cols2, diff_lims2, dat = noAnyth)
 
 
 noIgnit = 100 * noIgnit / control
