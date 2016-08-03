d2e89111a455d702dd86c6b9ae60c8a90f401635
douglask3 <douglas.i.kelley@gmail.com>
2016-08-03 11:43:10 +0100

    adding sensitivity functions to main model

--- src/LimFIRE/LimFIRE.r
+++ src/LimFIRE/LimFIRE.r
@@ -1,16 +1,29 @@
 LimFIRE <- function(fuel, moisture_live, moisture_dead,
                     lightning, human_ignitions,
                     agriculture, popdens,
-                    f1, f2, M, m1, m2, H, i1, P, s1, s2, fireOnly = FALSE) {
-
+                    f1, f2, M, m1, m2, H, i1, P, s1, s2, 
+                    fireOnly = FALSE, sensitivity = FALSE) {
+    
+    if (sensitivity) {
+        FUN.fuel       = dLimFIRE.fuel
+        FUN.moisture   = dLimFIRE.moisture
+        FUN.ignitions  = dLimFIRE.ignitions
+        FUN.supression = dLimFIRE.supression    
+    } else {
+        FUN.fuel       = LimFIRE.fuel
+        FUN.moisture   = LimFIRE.moisture
+        FUN.ignitions  = LimFIRE.ignitions
+        FUN.supression = LimFIRE.supression            
+    }
+        
     moisture   = (moisture_live + M * moisture_dead) / (1 + M)
     ignitions  = (lightning + H * human_ignitions) / (1 + H)
     supression = (agriculture + P * popdens) / (1 + P)
     
-    Fuel       = LimFIRE.fuel      (fuel      , f1, f2)
-    Moisture   = LimFIRE.moisture  (moisture  , m1, m2)
-    Ignitions  = LimFIRE.ignitions (ignitions , i1    )
-    Supression = LimFIRE.supression(supression, s1, s2)
+    Fuel       = FUN.fuel      (fuel      , f1, f2)
+    Moisture   = FUN.moisture  (moisture  , m1, m2)
+    Ignitions  = FUN.ignitions (ignitions , i1    )
+    Supression = FUN.supression(supression, s1, s2)
 
     Fire = (1 - Fuel) * (1 - Moisture) * (1 - Ignitions) * (1 - Supression)
     
@@ -22,3 +35,8 @@ LimFIRE.fuel       <- function(...) 1-f1(...)
 LimFIRE.moisture   <- function(...)   f1(...)
 LimFIRE.ignitions  <- function(...) 1-f2(...)
 LimFIRE.supression <- function(...)   f1(...)
+
+dLimFIRE.fuel       <- function(...) 1-df1(...)
+dLimFIRE.moisture   <- function(...)   df1(...)
+dLimFIRE.ignitions  <- function(...) 1-df2(...)
+dLimFIRE.supression <- function(...)   df1(...)
--- src/LimFIRE/fn.r
+++ src/LimFIRE/fn.r
@@ -1,6 +1,29 @@
 f1 <- function(x, a, b) 1/(1 + a * exp(-b * x))
 
+
+df1 <- function(x, a, b, d = 0.1) {       
+    df1_fun <- function(i) f1(i - d, a, b) - f1(i + d, a, b)
+    
+    
+    xhalf =  -(1/b) * log(1/a)
+    dhalf = df1_fun(xhalf)
+    
+    dx    = df1_fun(x)
+    
+    return(dx / dhalf)
+}
+
 f2 <- function(x, a) {
     x = x * a
     return(x / (x + 1))
+}
+
+df2 <- function(x, a, d = 0.1) {       
+    df2_fun <- function(i) f2(i - d, a) - f2(i + d, a)
+    
+    
+    dhalf = df2_fun(0)    
+    dx    = df2_fun(x)
+    
+    return(dx / dhalf)
 }
